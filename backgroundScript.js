54d57f12b09da12938bcef417405d5342150d838
dmnbars <dmnbars@gmail.com>
2023-06-09 15:22:55 +0300

    add check for window focus before closing tab to prevent accidental tab closure

--- backgroundScript.js
+++ backgroundScript.js
@@ -1,8 +1,12 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete' && checkUrl(tab.url)) {
         setTimeout(function() {
-            chrome.tabs.remove(tab.id);
-        }, 100);
+            chrome.windows.get(tab.windowId, null, (window) => {
+                if (!window.focused) {
+                    chrome.tabs.remove(tab.id)
+                }
+            })
+        }, 500);
     }
 });
 
